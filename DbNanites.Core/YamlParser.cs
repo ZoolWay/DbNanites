f1c107a608899875439adcb833682dc01eb2c28b
ZoolWay <zoolway@gmail.com>
2014-02-02 17:13:23 +0100

    NEW: Added YamlParser stub

---
+++ DbNanites.Core/YamlParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbNanites.Core
+{
+    class YamlParser
+    {
+
+        public YamlParser()
+        {
+        }
+
+        public YamlDefinition Parse(string filename)
+        {
+            using (FileStream fs = File.OpenRead(filename))
+            {
+                return Parse(fs);
+            }
+        }
+
+        public YamlDefinition Parse(Stream yamlInputStream)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
