584d4b2912d54b89c8435039e15135f8753b8362
ZoolWay <zoolway@gmail.com>
2014-02-15 00:02:21 +0100

    NEW: Added predefined filters to the MigrationRunner: All, OnlyNonCritical

--- DbNanites.Core/MigrationRunner.cs
+++ DbNanites.Core/MigrationRunner.cs
@@ -17,6 +17,17 @@ namespace DbNanites.Core
 
         }
 
+        /// <summary>
+        /// Filter method can be used with Run() to get only migrations to run which cannot loose data.
+        /// </summary>
+        public Func<IMigrationTask, bool> OnlyNonCritical = (migrationTask) => { return (!migrationTask.CanLooseData); };
+
+        /// <summary>
+        /// Filter method can be used with Run() to run all migrations. Although in that case the filter
+        /// parameter could just be omitted.
+        /// </summary>
+        public Func<IMigrationTask, bool> All = (migrationTask) => { return true; };
+
         public void Run(IAnalyzeResult analyzeResult, Func<IMigrationTask, bool> filterDelegate = null)
         {
             throw new NotImplementedException();
